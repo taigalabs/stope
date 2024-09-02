8bc245c5eb0cef5c85fb41aa1cc9d3c5fb021f64
Elden Park <eldeniyenden@gmail.com>
2024-09-03 05:01:36 +0900

    o

--- source/stope_web_app/src/containers/home/HomeContainer.tsx
+++ source/stope_web_app/src/containers/home/HomeContainer.tsx
@@ -8,10 +8,10 @@ import "./reactCOIServiceWorker";
 import ZkappWorkerClient from "./zkappWorkerClient";
 
 let transactionFee = 0.1;
-// const ZKAPP_ADDRESS = "B62qpXPvmKDf4SaFJynPsT6DyvuxMS9H1pT4TGonDT26m599m7dS9gP";
+const ZKAPP_ADDRESS = "B62qpXPvmKDf4SaFJynPsT6DyvuxMS9H1pT4TGonDT26m599m7dS9gP";
 // const ZKAPP_ADDRESS = "B62qkbCH6jLfVEgR36UGyUzzFTPogr2CQb8fPLLFr6DWajMokYEAJvX";
 // const ZKAPP_ADDRESS = "B62qqLv3vCRyfEquV8Us6MFkbeyD6wHqn63qCPJSyhFQnSxJkV7xtK6";
-const ZKAPP_ADDRESS = "B62qp31xbGLbFVYxH23yFgqwW45sPteNMJvioQwHnE9g1QUGj18H3Yr";
+// const ZKAPP_ADDRESS = "B62qp31xbGLbFVYxH23yFgqwW45sPteNMJvioQwHnE9g1QUGj18H3Yr";
 
 export const HomeContainer = () => {
   const [state, setState] = useState({
