47000f9d2dfbeaf51e71c729bba0e80a488d34d0
Elden Park <eldeniyenden@gmail.com>
2024-09-03 02:16:23 +0900

    o

---
+++ source/stope_entities/src/sto.ts
@@ -0,0 +1,20 @@
+export interface STO {
+  symbol: string;
+  isin: string;
+  totalSupply: string;
+  issuerName: string;
+  name: string;
+  pledgedBalances: string;
+  isTrust: string;
+  trustName: string;
+  limitedBalances: string;
+  decimals: string;
+  lockedBalances: string;
+  balances: string;
+  issuable: string;
+  paused: string;
+  issuer: string;
+  InstitutionBalances: string;
+  usable: string;
+  whitelistContractAddress: string;
+}
