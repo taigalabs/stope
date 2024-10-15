2fd5115936c8c0d10e22e11f76406f2f9bb20ae1
Elden Park <eldeniyenden@gmail.com>
2024-10-16 05:45:54 +0900

    o

--- source/stope_web_app/src/views/asset_item/asset_item_view.tsx
+++ source/stope_web_app/src/views/asset_item/asset_item_view.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { mockAssets } from "@taigalabs/stope-mock-data";
+
+import styles from "./asset_item_view.module.scss";
 
 export const AssetItemView: React.FC<AssetItemViewProps> = ({ idx }) => {
-  return <div>{idx}</div>;
+  const asset = mockAssets[Number(idx)];
+
+  return asset && <div className={styles.wrapper}>{asset.isin}</div>;
 };
 
 export interface AssetItemViewProps {
