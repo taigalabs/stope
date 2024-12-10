90cfd4ac25c1545b1ad31d1a19e9c6066701a9e6
Elden Park <eldeniyenden@gmail.com>
2024-12-10 18:47:04 +0900

    o

---
+++ source/stope_bridge_proof/src/main.ts
@@ -0,0 +1,3 @@
+// https://minascan.io/devnet/tx/5JvRtcf4WwWW2ZTDtT7ngCbdLtfsaJm6LiKG8GRqAQvnnGWiRyaR?type=zk-tx
+export const zkappAddress =
+  "B62qo2EZR12VjuyCoFZJU5ZNm8SFFJXz1aGS5hfhoiPgc1oe1K8hTKh";
